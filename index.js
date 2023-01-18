5abee3b9c8aa72a859e563bf32ad281705c40fc5
Ciobanu Alexandru <ialoveni94@gmail.com>
2023-01-18 08:51:05 +0200

    read

---
+++ index.js
@@ -0,0 +1,66 @@
+//1
+/*var et = prompt("Introdu numarul etajului");
+var sum = 20;
+if (et == 1){
+    document.write("Gratis");
+} else if (et == 2);{
+    document.write('${sum} lei');
+} else if (et == 3){
+    document.write('${sum+20} lei');
+} else if (et == 4){
+    document.write('${sum+40} lei');
+} else if (et == 5){
+    document.write('${sum+70} lei');
+} else if (et == 6){
+    document.write('${sum+100} lei');
+} else if (et == 7){
+    document.write('${sum+130} lei');
+} else if (et == 8){
+    document.write('${sum+170} lei');
+} else if (et == 9){
+    document.write('${sum+210} lei');
+} else if (et == 10){
+    document.write('${sum+250} lei');
+} 
+*/
+
+//2
+/*let nr = prompt("Introdu un nr");
+let nrRandom = Math.random(Math.random() * (10 - 1 + 1) + 1);
+if (nr !== nrRandom) {
+    document.write("Nu ati ghicit numarul nu se potriveste");
+    console.log(nrRandom);
+}*/
+
+//3
+let luna = +prompt("Introdu un nr de la 1 la 12");
+switch (luna) {
+    case 1: document.write("Ianuarie");
+    break;
+    case 2: document.write("Februarie");
+    break;
+    case 3: document.write("Martie");
+    break;
+    case 4: document.write("Aprilie");
+    break;
+    case 5: document.write("Mai");
+    break;
+    case 6: document.write("Iunie");
+    break;
+    case 7: document.write("Iulie");
+    break;
+    case 8: document.write("August");
+    break;
+    case 9: document.write("Septembrie");
+    break;
+    case 10: document.write("Octombrie");
+    break;
+    case 11: document.write("Noiembrie");
+    break;
+    case 12: document.write("Decembrie");
+    break;
+}
+
+
+    
+    
